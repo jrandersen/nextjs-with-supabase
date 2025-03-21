1a2ca1c42c7a6ee3f1b6a5ae5c926f62b98abe2b
jandersen <andersen.ja@gmail.com>
2025-03-21 08:17:50 -0400

    update site names, titles & headings

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -30,7 +30,7 @@ export default async function ProtectedPage() {
         </pre>
       </div>
       <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
+        <h2 className="font-bold text-2xl mb-4">Supabase API call on Notes Table</h2>
         <FetchDataSteps />
       </div>
     </div>
